824e0770cdfa232e8f7d818b4fef96ce9cfacf3b
Jakub Widurek <jakubwidurek@gmail.com>
2022-10-03 14:45:26 +0200

    add redux-thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,29 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 
-export const setCategories = (categoriesArray) => ({
-  type: CATEGORIES_ACTION_TYPES.SET_CATEGORIES,
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+export const fetchCategoriesStart = () => ({
+  type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+});
+
+export const fetchCategoriesSuccess = (categoriesArray) => ({
+  type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
   payload: categoriesArray,
 });
+
+export const fetchCategoriesFailure = (error) => ({
+  type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+  payload: error,
+});
+
+export const fetchCategoriesStartAsync = () => {
+  return async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+      dispatch(fetchCategoriesFailure(error));
+    }
+  };
+};
--- src/store/store.js
+++ src/store/store.js
@@ -3,36 +3,33 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from 'redux';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import thunk from 'redux-thunk';
+
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-};
 
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user'],
+  whitelist: ['cart'],
 };
 
-const middleWares = [loggerMiddleware];
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk,
+].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
